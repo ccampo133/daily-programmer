1f1780567a2b9d1901906936e1597e7d79c5d831
Chris Campo <christopher.campo@aspect.com>
2015-01-27 19:58:45 -0500

    Finished 199-easy.

--- 199-easy/src/ccampo/Main.java
+++ 199-easy/src/ccampo/Main.java
@@ -1,8 +1,80 @@
 package ccampo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        final char[][][] numbers = {
+                { // Zero
+                        { ' ', '_', ' ' },
+                        { '|', ' ', '|' },
+                        { '|', '_', '|' },
+                },
+                { // One
+                        { ' ', ' ', ' ' },
+                        { ' ', '|', ' ' },
+                        { ' ', '|', ' ' },
+                },
+                { // Two
+                        { ' ', '_', ' ' },
+                        { ' ', '_', '|' },
+                        { '|', '_', ' ' },
+                },
+                { // Three
+                        { ' ', '_', ' ' },
+                        { ' ', '_', '|' },
+                        { ' ', '_', '|' },
+                },
+                { // Four
+                        { ' ', ' ', ' ' },
+                        { '|', '_', '|' },
+                        { ' ', ' ', '|' },
+                },
+                { // Five
+                        { ' ', '_', ' ' },
+                        { '|', '_', ' ' },
+                        { ' ', '_', '|' },
+                },
+                { // Six
+                        { ' ', '_', ' ' },
+                        { '|', '_', ' ' },
+                        { '|', '_', '|' },
+                },
+                { // Seven
+                        { ' ', '_', ' ' },
+                        { ' ', ' ', '|' },
+                        { ' ', ' ', '|' },
+                },
+                { // Eight
+                        { ' ', '_', ' ' },
+                        { '|', '_', '|' },
+                        { '|', '_', '|' },
+                },
+                { // Nine
+                        { ' ', '_', ' ' },
+                        { '|', '_', '|' },
+                        { ' ', '_', '|' },
+                }
+        };
+
+        final List<String> lines = Files.readAllLines(Paths.get(args[0]));
+        final StringBuilder s = new StringBuilder();
+        for (final String line : lines) {
+            for (int row = 0; row < 3; ++row) {
+                for (final char numChar : line.toCharArray()) {
+                    final int num = Character.getNumericValue(numChar);
+                    for (int col = 0; col < 3; ++col) {
+                        s.append(numbers[num][row][col]);
+                    }
+                }
+                s.append("\n");
+            }
+        }
+
+        System.out.print(s);
     }
 }
