cc9320afd3c8c85dfc20e385253be3015063b444
Chris Campo <ccampo133@users.noreply.github.com>
2015-03-20 02:11:23 -0400

    Fixed some typos

--- 206-intermediate/sprinkler.py
+++ 206-intermediate/sprinkler.py
@@ -1,24 +1,24 @@
 import numpy as np
 
-
-# Generates a 2D bit array of dimensions (w, h), with a
+# Generates a 2D bit array of dimensions `h` by `w`, with a
 # disk of ones with radius `r`, centered at point (x, y).
 # All other points in the array are zero valued.
 def disk(r, x, y, h, w):
     ind = np.indices((h, w))
     return r ** 2.0 >= (ind[0] - y) ** 2.0 + (ind[1] - x) ** 2.0
 
+
 with open("input.txt", "r") as f:
     h, w, r = map(int, f.readline().split())
     # Convert x to 1 and everything else (.) to 0.
-    grid = [np.array(map(lambda c: 1 if c == 'x' else 0, f.readline()[:-1])) for j in range(h)]
+    grid = np.array([map(lambda c: 1 if c == 'x' else 0, f.readline()[:-1]) for j in range(h)])
 
 max = 0
 for i in range(h):
     for j in range(w):
-        sum = (grid * disk(r, (i, j), (h, w))).sum()
+        sum = (grid * disk(r, j, i, h, w)).sum()
         if sum >= max:
             max = sum
             x, y = j, i
 
-print((x, y), max)
+print(x, y)
