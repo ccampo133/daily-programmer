a6457ea69cecbad14a1652c16f90ac104c4ad5a4
Chris Campo <ccampo133@users.noreply.github.com>
2015-04-09 00:14:48 -0400

    Finished 209-intermediate

--- 209-intermediate/src/SentencePacker.scala
+++ 209-intermediate/src/SentencePacker.scala
@@ -1,6 +1,8 @@
-/**
- * @author Chris Campo
- */
+import scala.io.StdIn
+
 object SentencePacker extends App {
-  
+  val s = StdIn.readLine().filter(_ != ' ')
+  val l = (Math.sqrt(s.length).floor.toInt to s.length).find(s.length % _ == 0).getOrElse(s.length)
+  println("1 1")
+  s.grouped(l).foreach(println)
 }
