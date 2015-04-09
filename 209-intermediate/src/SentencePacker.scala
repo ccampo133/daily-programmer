d70c7251228724bf4e008c128d9a7d2e23a56ad9
Chris Campo <ccampo133@users.noreply.github.com>
2015-04-09 00:34:00 -0400

    Print the string zig-zag this time :P

--- 209-intermediate/src/SentencePacker.scala
+++ 209-intermediate/src/SentencePacker.scala
@@ -4,5 +4,5 @@ object SentencePacker extends App {
   val s = StdIn.readLine().filter(_ != ' ')
   val l = (Math.sqrt(s.length).floor.toInt to s.length).find(s.length % _ == 0).getOrElse(s.length)
   println("1 1")
-  s.grouped(l).foreach(println)
+  s.grouped(l).zipWithIndex.foreach { case (grp, i) => if (i % 2 == 0) println(grp) else println(grp.reverse) }
 }
