aaa4b5ee57139f89002807ed1f4e829fb7e32570
Chris Campo <ccampo133@users.noreply.github.com>
2015-04-27 22:17:44 -0400

    Added 212-easy

---
+++ 212-easy/src/Rovarspraket.scala
@@ -0,0 +1,18 @@
+import scala.io.StdIn
+
+object Rovarspraket extends App {
+  val input = StdIn.readLine()
+  val vowels = Set('A', 'E', 'I', 'O', 'U', 'Y', 'Å', 'Ä', 'Ö')
+  println(s"Encoded: ${encode(input)}")
+  println(s"Decoded: ${decode(input)}")
+
+  def isConsonant(c: Char) = Character.isLetter(c) && !vowels.contains(c.toUpper)
+
+  def encode(s: String) = s.map(c => if (isConsonant(c)) c + "o" + c.toLower else c.toString).mkString
+
+  def decode(s: String): String = {
+    if (s.length == 0) return ""
+    if (isConsonant(s(0))) s(0) + decode(s.drop(3))
+    else s(0) + decode(s.tail)
+  }
+}
